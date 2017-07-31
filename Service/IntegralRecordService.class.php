79a6ebe1c446ff84c57ddc775359328fb93536ef
Jayin Ton <tonjayin@gmail.com>
2017-07-31 15:10:25 +0800

    refactor

--- Service/IntegralRecordService.class.php
+++ Service/IntegralRecordService.class.php
@@ -7,13 +7,15 @@ use Record\Records\IntegralRecord;
 class IntegralRecordService extends RecordService {
     /**
      * 创建一个交易记录
-     * @param        $to  流入者id
-     * @param        $target_type 记录产生类型
-     * @param        $target 记录产生唯一标识
-     * @param int    $income 收入
-     * @param int    $pay 支出
-     * @param int    $status 记录状态
+     *
+     * @param string       $to          流入者id
+     * @param string       $target_type 记录产生类型
+     * @param string       $target      记录产生唯一标识
+     * @param int    $income      收入
+     * @param int    $pay         支出
+     * @param int    $status      记录状态
      * @param string $detail
+     * @param string $remark
      * @return array
      */
     static function createIntegralRecord(
@@ -26,13 +28,13 @@ class IntegralRecordService extends RecordService {
         $detail = '',
         $remark = ''
     ) {
-        $recored = new IntegralRecord($to, $target_type, $target);
-        $recored->setIncome($income);
-        $recored->setPay($pay);
-        $recored->setStatus($status);
-        $recored->setDetail($detail);
-        $recored->setRemark($remark);
+        $record = new IntegralRecord($to, $target_type, $target);
+        $record->setIncome($income);
+        $record->setPay($pay);
+        $record->setStatus($status);
+        $record->setDetail($detail);
+        $record->setRemark($remark);
 
-        return self::createRrcord($recored);
+        return self::createRrcord($record);
     }
 }
--- Service/TradeRecordService.class.php
+++ Service/TradeRecordService.class.php
@@ -7,9 +7,9 @@ use Record\Records\TradeRecord;
 class TradeRecordService extends RecordService {
     /**
      * 创建一个交易记录
-     * @param        $to  流入者id
-     * @param        $target_type 记录产生类型
-     * @param        $target 记录产生唯一标识
+     * @param string   $to  流入者id
+     * @param string       $target_type 记录产生类型
+     * @param string       $target 记录产生唯一标识
      * @param int    $income 收入
      * @param int    $pay 支出
      * @param int    $status 记录状态
@@ -25,12 +25,12 @@ class TradeRecordService extends RecordService {
         $status = RecordModel::STATUS_VAILD,
         $detail = ''
     ) {
-        $trade_recored = new TradeRecord($to, $target_type, $target);
-        $trade_recored->setIncome($income);
-        $trade_recored->setPay($pay);
-        $trade_recored->setStatus($status);
-        $trade_recored->setDetail($detail);
+        $trade_record = new TradeRecord($to, $target_type, $target);
+        $trade_record->setIncome($income);
+        $trade_record->setPay($pay);
+        $trade_record->setStatus($status);
+        $trade_record->setDetail($detail);
 
-        return self::createRrcord($trade_recored);
+        return self::createRrcord($trade_record);
     }
 }
