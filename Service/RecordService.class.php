687f4c509e95f53e38486ada148bf15d21c4c1cb
Jayin Ton <tonjayin@gmail.com>
2017-07-31 15:08:12 +0800

    新增事务

--- Service/RecordService.class.php
+++ Service/RecordService.class.php
@@ -14,12 +14,14 @@ class RecordService extends BaseService {
      */
     static function createRrcord(Record $record) {
         //获取上一条合法的record id
-        $where['status'] = RecordModel::STATUS_VAILD;
         $where['to'] = $record->getTo();
-        $where['to_type'] = $record->getToType();
         $last_vaild_record = M($record->table_name)->where($where)->order('id desc')->find();
         $last_vaild_balance = self::getBalance($record)['data']; //获取最近的余额信息
 
+        $Model = M();
+        $Model->startTrans();
+
+        $balance = $last_vaild_balance + $record->getIncome() - $record->getPay();
         $data = [
             'parent_id' => $last_vaild_record ? $last_vaild_record['id'] : 0,
             'to' => $record->getTo(),
@@ -30,7 +32,7 @@ class RecordService extends BaseService {
             'target_type' => $record->getTargetType(),
             'income' => $record->getIncome(),
             'pay' => $record->getPay(),
-            'balance' => ($last_vaild_balance + $record->getIncome() - $record->getPay()),//计算当前记录的余额
+            'balance' => $balance,//计算当前记录的余额
             'detail' => $record->getDetail(),
             'status' => $record->getStatus(),
             'create_time' => time(),
@@ -38,10 +40,12 @@ class RecordService extends BaseService {
         ];
         $result = M($record->table_name)->add($data);
         if ($result) {
+            $Model->commit();
             return self::createReturn(true, $result);
+        }else{
+            $Model->rollback();
+            return self::createReturn(false, null, '操作失败');
         }
-
-        return self::createReturn(false, null, '操作失败');
     }
 
     /**
@@ -50,7 +54,29 @@ class RecordService extends BaseService {
      * @param Record $record
      * @return array
      */
-    static function getBalance(Record $record) {
+    static function getBalance(Record $record){
+        $where = [
+            'to' => $record->getTo(),
+            'status' => RecordModel::STATUS_VAILD
+        ];
+        $record = M($record->table_name)->where($where)->order('id DESC')->find();
+
+        if(empty($record)){
+            $balance = 0;
+        }else{
+            $balance = $record['balance'];
+        }
+
+        return self::createReturn(true, $balance);
+    }
+
+    /**
+     * 获取当前 to（记录流入者）余额 (实时计算)
+     *
+     * @param Record $record
+     * @return array
+     */
+    static function getBalanceByCal(Record $record) {
         $where = [
             'to' => $record->getTo(),
             'to_type' => $record->getToType(),
